57092f636b18b5a3b25dc1f569b7181518c53724
Abedalrhman Rizk <146388592+abedalrhmanrizk@users.noreply.github.com>
2023-11-03 03:33:43 +0200

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,23 +13,8 @@ function Footer() {
       <div className={styles.content}>
         <div>
           <Logo mobileAlignment="center" />
-          {/*
-            NOTE: If you'd like to build your blog on top
-            of this code, the license requires that you leave
-            this paragraph untouched. Check out LICENSE.md
-            for more information.
-          */}
           <p className={styles.attribution}>
-            Blog template created by{' '}
-            <a href="https://www.joshwcomeau.com/">
-              Josh W. Comeau
-            </a>
-            . Check out{' '}
-            <a href="https://www.joyofreact.com/">
-              The Joy of React
-            </a>{' '}
-            to learn how to build dynamic React apps like
-            this one!
+            Blog template created by Josh W. Comeau
           </p>
         </div>
         <nav>
@@ -39,13 +24,13 @@ function Footer() {
               <Link href="/rss">RSS feed</Link>
             </li>
             <li>
-              <Link href="/todo">Terms of Use</Link>
+              <Link href="/terms">Terms of Use</Link>
             </li>
             <li>
-              <Link href="/todo">Privacy Policy</Link>
+              <Link href="/privacy">Privacy Policy</Link>
             </li>
             <li>
-              <a href="https://twitter.com/JoshWComeau">
+              <a href="https://twitter.com/abedalrhmanrizk">
                 Twitter
               </a>
             </li>
